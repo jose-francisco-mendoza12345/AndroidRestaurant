de07ae9f29b67fd7da824c747d7b2e193b678722
Nicolas <oscuro959@gmail.com>
2020-10-13 00:07:39 +0800

    Agregar o Subir Imagenes

--- app/src/main/java/com/example/proyectooriginal/RestauranteActivity.java
+++ app/src/main/java/com/example/proyectooriginal/RestauranteActivity.java
@@ -1,15 +1,20 @@
 package com.example.proyectooriginal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class RestauranteActivity extends AppCompatActivity {
     private EditText nombre, nit, propietario, calle, telf, lon, lat, fecha;
+    private ImageView logo, restaurante,AUX;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +28,36 @@ public class RestauranteActivity extends AppCompatActivity {
         lon=(EditText) findViewById(R.id.TextoLog);
         lat=(EditText) findViewById(R.id.TextoLat);
         fecha=(EditText) findViewById(R.id.TextFecha);
+
+        logo=(ImageView) findViewById(R.id.FotoLogo);
+        restaurante=(ImageView) findViewById(R.id.FotoRestaurante);
     }
+
+    public void SubirLogo(View view)
+    {
+        AUX=logo;
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicacion"),10);
+    }
+    public void SubirRestaurante(View view)
+    {
+        AUX=restaurante;
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicacion"),10);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK)
+        {
+            Uri path=data.getData();
+            AUX.setImageURI(path);
+            Toast.makeText(this, "Imagen Insertada Correctamente", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     public  void Guargar(View view)
     {
