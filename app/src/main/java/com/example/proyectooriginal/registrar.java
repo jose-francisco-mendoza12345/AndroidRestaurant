5d125ca29d137893b92c04e9c65dc12f64ab7764
jose-francisco-mendoza12345 <franciscopmr12345@gmail.com>
2020-10-19 14:42:25 -0500

    una parte mas

--- app/src/main/java/com/example/proyectooriginal/registrar.java
+++ app/src/main/java/com/example/proyectooriginal/registrar.java
@@ -19,32 +19,26 @@ import org.json.JSONObject;
 import cz.msebera.android.httpclient.Header;
 
 public class registrar extends AppCompatActivity{
-    EditText email1,pass1;
-    Button btn1;
-    EditText nick,ci,calle,telefono,email2,pass2;
+
+  //  Button btn1;
+    EditText nick,ci,calle,telefono,Tipo,email2,pass2,Fecha;
     Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
-        email1=findViewById(R.id.log_email);
-        pass1=findViewById(R.id.log_pass);
-        btn1=findViewById(R.id.log_btn);
 
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sendLogin();
-            }
-        });
+
 
         nick=findViewById(R.id.reg_nick);
         ci=findViewById(R.id.reg_ci);
         calle=findViewById(R.id.reg_calle);
         telefono=findViewById(R.id.reg_telefono);
+        Tipo=findViewById(R.id.reg_Tipo);
         email2=findViewById(R.id.reg_email);
         pass2=findViewById(R.id.reg_password);
+        Fecha=findViewById(R.id.reg_Fecha);
         btn2=findViewById(R.id.reg_send);
 
         btn2.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +51,8 @@ public class registrar extends AppCompatActivity{
         });
 
     }
+
+ /*
     private void sendLogin(){
         AsyncHttpClient client=new AsyncHttpClient();
         RequestParams req=new RequestParams();
@@ -75,16 +71,18 @@ public class registrar extends AppCompatActivity{
             }
         });
     }
-
+*/
     private  void sendRegister(){
         AsyncHttpClient client=new AsyncHttpClient();
         RequestParams req=new RequestParams();
         req.put("nick",nick.getText().toString());
-        req.put("edad",Integer.parseInt(ci.getText().toString()));
-        req.put("tipò",calle.getText().toString());
-        req.put("tipò",telefono.getText().toString());
+        req.put("ci",Integer.parseInt(ci.getText().toString()));
+        req.put("calle",calle.getText().toString());
+        req.put("telefono",telefono.getText().toString());
+        req.put("Tipo",Tipo.getText().toString());
         req.put("email",email2.getText().toString());
         req.put("password",pass2.getText().toString());
+        req.put("Fecha",Fecha.getText().toString());
         client.post(Endpoints.LOGIN_SERVICE,req,new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
